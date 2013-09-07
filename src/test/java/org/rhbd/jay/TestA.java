74ce52a974842dbbb281277a92d578d16b9730a7
jayunit100 <jayunit100@gmail.com>
2013-09-07 17:26:34 -0400

    tests

---
+++ src/test/java/org/rhbd/jay/TestA.java
@@ -0,0 +1,12 @@
+package org.rhbd.jay;
+
+import org.junit.Test;
+import org.rhbd.jay.ClusterDudJob;
+
+public class TestA {
+
+    @Test
+    public void test1() throws Exception{
+        ClusterDudJob.main(null);
+    }
+}
---
+++ src/test/java/your/simple/java/gid/TestA.java
@@ -0,0 +1,12 @@
+package your.simple.java.gid;
+
+import org.junit.Test;
+import org.rhbd.jay.ClusterDudJob;
+
+public class TestA {
+
+    @Test
+    public void test1() throws Exception{
+        ClusterDudJob.main(null);
+    }
+}
