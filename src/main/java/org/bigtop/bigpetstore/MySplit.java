08290781dcdc95a70161564bc0f89584e76a742c
Matt Fenwick <mfenwick100@gmail.com>
2013-10-17 22:54:41 -0400

    re-implement Writer interface in split to fix NullPointerException problem

--- src/main/java/org/bigtop/bigpetstore/MySplit.java
+++ src/main/java/org/bigtop/bigpetstore/MySplit.java
@@ -1,19 +1,37 @@
 package org.bigtop.bigpetstore;
 
 import java.io.IOException;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputSplit;
 
 /**
  * What does an `InputSplit` actually do?
  * From the Javadocs, it looks like ... absolutely nothing.
+ *
+ * Note: for some reason, you *have* to implement Writable,
+ * even if your methods do nothing, or you will got strange
+ * and un-debuggable null pointer exceptions.
  */
-public class MySplit extends InputSplit {
+public class MySplit extends InputSplit implements Writable {
 	
 	private static int count = 1;
 
 	public MySplit() {
 		System.out.println("MySplit constructor called: " + count);
 		count++; // is this synchronized?  who knows ....
+	}
+	
+	public void readFields(DataInput arg0) throws IOException {
+//		throw new RuntimeException("unimplemented method MySplitter.readFields");
+		System.out.println("calling MySplitter.readFields ... for some reason");
+	}
+
+	public void write(DataOutput arg0) throws IOException {
+//		throw new RuntimeException("unimplemented method MySplitter.write");
+		System.out.println("calling MySplitter.write ... for some reason");
 	}
 	
 	@Override
